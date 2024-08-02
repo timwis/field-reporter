8005dbbe64c00918132ea79fb81c87b28b392520
timwis <tim@timwis.com>
2024-08-02 13:38:09 +0100

    Add tailwindcss, headlessui, and heroicons

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,8 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 import PWABadge from './components/PWABadge.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/favicon.svg" class="logo" alt="Field Reporter logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Field Reporter" />
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
   <PWABadge />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
